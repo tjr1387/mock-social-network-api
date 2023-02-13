65345f596bf33c7be6a4b2ee39255a1d2dddd895
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-13 17:47:35 -0500

    Complete User model, first pass

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require('mongoose');
+
+// Schema for User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email syntax invalid.'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    // Include virtuals in response by default
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual for friend count (length of the assoc. friends array)
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return this.friends.length;
+  });
+
+// Initialize model
+const User = model('user', userSchema);
+
+module.exports = User;
