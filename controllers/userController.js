22fe3f9e50f18cdd10f520d55aa0e03eee2cca5b
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-13 16:58:28 -0500

    Add starter files

---
+++ controllers/userController.js
@@ -0,0 +1,43 @@
+const { User, Application } = require('../models');
+
+// still has legacy code from exercise! 'Application' will be removed
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+
+  //update user controller needed!
+
+  // Delete a user and associated apps
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Application.deleteMany({ _id: { $in: user.applications } })
+      )
+      .then(() => res.json({ message: 'User and associated apps deleted!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+};
