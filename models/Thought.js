daf3c102df161a64d60abaf3d9b30396698440cf
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-13 18:10:34 -0500

    Add first pass of Thought model

---
+++ models/Thought.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema for reaction subdocument (maybe make this its own file and import it?)
+const reactionSchema = new Schema(
+
+);
+
+
+// Schema for Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // will need a getter on this in order to format
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],  //embedded subdoc
+  },
+  {
+    // Include virtuals in response by default
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  },
+);
+
+// Virtual for reaction count (length of the assoc. reactions array)
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  });
+
+// Initialize model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
