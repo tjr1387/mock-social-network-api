286d64fe31b37e9b2ab498d7fc545c4ef21b09fc
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-14 18:29:20 -0500

    Add first pass of thought rotues/controller

---
+++ controllers/thoughtController.js
@@ -0,0 +1,55 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+  // Get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Get a single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.ThoughtId })
+      .select('-__v')
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new thought
+    // need to add a 'push to assoc. user's array' bit
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // update a thought
+  updateThought(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this id!' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+  // Delete a thought (and remove from assoc. users array)
+    // this is still legacy and needs fixing
+  deleteThought(req, res) {
+    Thought.findOneAndDelete({ _id: req.params.ThoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .then(() => res.json({ message: 'Thought deleted and removed from user!' }))
+      .catch((err) => res.status(500).json(err));
+  },
+};
