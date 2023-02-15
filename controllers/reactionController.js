8637f5f8cb6132f7d27a223571e561fe1e97841e
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-15 11:22:48 -0500

    Add controller/routing for reactions

---
+++ controllers/reactionController.js
@@ -0,0 +1,12 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+  // a POST which adds a reaction to a user's thought
+  addReaction(req, res) {
+    
+  },
+  // a DELETE which removes a reaction from a user's thought
+  removeReaction(req, res) {
+
+  }
+}
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -6,11 +6,18 @@ const {
   updateThought,
   deleteThought
 } = require('../../controllers/thoughtController');
+const {
+  addReaction,
+  removeReaction
+} = require('../../controllers/reactionController');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
 
 module.exports = router;
