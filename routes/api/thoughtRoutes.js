7c213eb555dd4fd7e8d5841e49d70f7db144886b
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-15 12:07:15 -0500

    Adjust thoughtRoutes formatting

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -12,12 +12,22 @@ const {
 } = require('../../controllers/reactionController');
 
 // /api/thoughts
-router.route('/').get(getThoughts).post(createThought);
+router
+  .route('/')
+  .get(getThoughts)
+  .post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+router
+  .route('/:thoughtId/reactions')
+  .post(addReaction)
+  .delete(removeReaction);
 
 module.exports = router;
