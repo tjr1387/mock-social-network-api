0c93d18960cb722a0e610c5024d75f7edc1bcc85
Hair_Trigg3r <tylerjreimer@gmail.com>
2023-02-15 10:54:33 -0500

    Complete addFriend functionality

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -2,7 +2,18 @@ const { User } = require('../models');
 
 module.exports = {
   // a POST which adds another user to the user's friends array
-  addFriend (req, res) {
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) => 
+        !user
+          ? res.status(404).json({ message: 'No user found with that ID'})
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
 
   },
   // a DELETE which removes a user from the user's friends array
